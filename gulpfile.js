781376e32401a0bb06cd70632c7e75af2245d897
tianzhen <340349237@qq.com>
2019-10-14 14:33:38 +0800

    feat: 增加增量编译

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,8 @@ const postcss = require('gulp-postcss'); // 处理css，配合autoprefixer使用
 const preprocess = require('gulp-preprocess'); // 注入环境变量
 const del = require('del');
 const eslint = require('gulp-eslint');
+const changed = require('gulp-changed');
+const debug = require('gulp-debug');
 
 // 编译样式
 gulp.task('build:style', () => {
@@ -19,6 +21,8 @@ gulp.task('build:style', () => {
     'src/miniprogram/app.less'
   ], {base: 'src/miniprogram'})
     .pipe(plumber())
+    .pipe(changed('dist', { extension: '.wxss' }))
+    .pipe(debug({title: '编译'}))
     .pipe(less())
     .pipe(postcss([autoprefixer(['iOS >= 8', 'Android >= 4.1'])]))
     .pipe(
@@ -46,6 +50,8 @@ gulp.task('build:main', gulp.series('build:style', () => {
     '!src/project.config.json', // 配置文件不写入到dist文件夹，开发时需手动拷贝到dist文件夹中 !!!
   ], {base: 'src/miniprogram', allowEmpty: true})
     .pipe(plumber())
+    .pipe(changed('dist'))
+    .pipe(debug({ title: '编译' }))
     .pipe(preprocess()) // 这个插件会影响静态资源
     .pipe(gulp.dest('dist'));
 }));
@@ -56,6 +62,7 @@ gulp.task('build:assets', () => {
     "src/miniprogram/assets/**/*"
   ], { base: 'src/miniprogram', allowEmpty: true })
     .pipe(plumber())
+    .pipe(changed('dist'))
     .pipe(gulp.dest('dist'));
 });
 
@@ -79,7 +86,7 @@ gulp.task('build', gulp.parallel('build:main', 'build:assets'));
 
 // 监听文件(若文件修改则执行相关的任务)
 function watch() {
-  let watcher = gulp.watch('src/**', gulp.parallel('build:main', 'build:assets', 'lint'), cb => cb());
+  let watcher = gulp.watch('src/**', { ignoreInitial: false }, gulp.parallel('build:main', 'build:assets', 'lint'), cb => cb());
   watcher.on('all', (event, path, stats) => {
     console.log('File ' + path + ' was ' + event + ', running tasks...');
   });
